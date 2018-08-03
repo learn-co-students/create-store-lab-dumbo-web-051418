d1ae41890d0f9352f7e44f7c59a882fa48f418ff
pablopawpaw <lpault29@gmail.com>
2018-08-03 12:08:34 -0400

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class UserInput extends Component {
   state = {
@@ -12,23 +13,31 @@ class UserInput extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
-    this.props.store.dispatch({
-      type: 'ADD_USER',
-      user: this.state
-    })
+    this.props.addUser(this.state)
   }
 
   render() {
-
     return(
       <div>
         <form onSubmit={(e) => this.handleSubmit(e)}>
           <input type='text' name='username' value={this.state.username} onChange={(e) => this.handleInputChange(e)} />
           <input type='text' name='hometown' value={this.state.hometown} onChange={(e) => this.handleInputChange(e)} />
+          <input type='submit' />
         </form>
       </div>
     );
   }
 };
 
-export default UserInput;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addUser: (user) => {
+      dispatch({
+        type: 'ADD_USER',
+        user
+      })
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(UserInput);
