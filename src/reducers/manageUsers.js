ae4a756a0a9c97a1da4347df8cf05011d1d8d7e8
github username <github email address>
2018-08-03 12:53:26 -0400

    coded userInput

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,20 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {
+  users: []
+}, action){
+  switch(action.type) {
+    case "ADD_USER":
+      return {users: [...state, action.user]}
+    default:
+      return state
+  }
 }
+
+//
+//
+// store.dispatch({
+//   type: 'ADD_USER',
+//   user: {
+//     username: 'bob',
+//     hometown: 'philly'
+//   },
+// });
